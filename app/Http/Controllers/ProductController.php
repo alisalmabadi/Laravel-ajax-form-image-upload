d670b2f0efeed542bc6c8c618b4a7a749eea6aab
Seyed Ali Salmabadi <s.alisalmabadi@yahoo.com>
2018-08-14 04:06:59 -1200

    edit!

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -16,34 +16,7 @@ class ProductController extends Controller
         $product->title=$data->title;
         $product->imageurl=$name;
         $product->save();
-       // $res=URL::asset("images/").$name;
         echo '../images/'.$name;
-        /*$image=Product::all();
-        $image=$image->where('imageurl',$name);
-        return view('product-add')->with('image',$image);*/
-/*
-$allowedExts = array("gif", "jpeg", "jpg", "png","GIF","JPEG","JPG","PNG");
-$temp = explode(".", $_FILES["file"]["name"]);
-$extension = end($temp);
-
-        if ((($_FILES["file"]["type"] == "image/gif")
-        || ($_FILES["file"]["type"] == "image/jpeg")
-        || ($_FILES["file"]["type"] == "image/jpg")
-        || ($_FILES["file"]["type"] == "image/pjpeg")
-        || ($_FILES["file"]["type"] == "image/x-png")
-        || ($_FILES["file"]["type"] == "image/png"))
-          && in_array($extension, $allowedExts)) {
-         if ($_FILES["file"]["error"] > 0) {
-            echo "0";
-        } else {
-        $target = "upload/";
-        move_uploaded_file($_FILES["file"]["tmp_name"], $target. $_FILES["file"]["name"]);
-        echo  "upload/" . $_FILES["file"]["name"];
-    }
-                } else {
-                 echo "0";
-                   }*/
-
     }
 
 }
