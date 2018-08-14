ab7eadbe59926386592be5ba1a3a6c655d5e0407
Seyed Ali Salmabadi <s.alisalmabadi@yahoo.com>
2018-08-14 04:04:02 -1200

    edited!

--- routes/web.php
+++ routes/web.php
@@ -10,34 +10,12 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
     return view('welcome');
 });
-
-Auth::routes();
-
+//Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
-
-
-Route::get('/add-note',[
-    'uses'=>'UserController@show',
-    'middleware'=>'auth'
+Route::get('/image/add',['uses'=>'ProductController@add','as'=>'product-add']);
+Route::post('/image/add',['uses'=>'ProductController@show',
+    'as'=>'product-add'
 ]);
-Route::post('/add-note','UserController@store')->name('add-note');
-Route::get('/index','UserController@index');
-Route::get('/product/add',['uses'=>'ProductController@add',
-    'middleware'=>'auth'
-])->name('product-add');
-Route::post('/product/add',['uses'=>'ProductController@show',
-    'middleware'=>'auth'
-])->name('product-add');
-
-Route::get('productdesigner','productDesigner@index');
-
-Route::get('productest',['uses'=>'ProductestController@index']);
-Route::post('productest',['uses'=>'ProductestController@post']);
-
-Route::post('saveproduct',['uses'=>'SaveProductController@save',
-    'middleware'=>'auth'
-    ]);
